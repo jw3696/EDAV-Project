9e1448bdae7c0193bdd560c99cc7611ba67073ea
Jialei Wang <jw3696@columbia.edu>
2018-12-10 15:49:21 -0500

    animation added

--- interactive/app.py
+++ interactive/app.py
@@ -138,3 +138,19 @@ def index():
 		return render_template("result.html", score = score, **udat, **inputDat)
 	return render_template("temp.html") 
 
+if __name__ == "__main__":
+	import click
+
+	@click.command()
+	@click.option('--debug', is_flag=True)
+	@click.option('--threaded', is_flag=True)
+	@click.argument('HOST', default='0.0.0.0')
+	@click.argument('PORT', default=8111, type=int)
+
+	def run(debug, threaded, host, port):
+		HOST, PORT = host, port
+		print("running on %s:%d" % (HOST, PORT))
+		app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+
+	run()
+
